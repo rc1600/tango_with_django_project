8a1e5ce30fd17a7d44f1f9346f5d129027a5788e
Robbie Copeland <2768763c@student.gla.ac.uk>
2024-01-31 20:06:52 +0000

    4.1

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -14,7 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+print(__file__)
+print(os.path.dirname(__file__))
+print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['<workspace>/tango_with_django_project/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
