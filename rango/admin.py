f877f25db5b9cada056a639f111b41d0eac96468
Robbie Copeland <2768763c@student.gla.ac.uk>
2024-02-01 22:04:21 +0000

    chapter 5.8

--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'views', 'likes')
+
+
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
