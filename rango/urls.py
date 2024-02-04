ac8699312c8b998c9d1f298490619a55c8c22ccf
Robbie Copeland <2768763c@student.gla.ac.uk>
2024-02-04 18:57:27 +0000

    chapter 6.3

--- rango/urls.py
+++ rango/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('category/<slug:category_name_slug>/',
-         views.show_category, name='show_category')
+         views.show_category, name='show_category'),
 ]
+
